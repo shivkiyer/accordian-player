ac63b07c5c14c6bccbc0cdedf81cbba6c0afc4ed
Shivkumar Iyer <pythonpowerelectronics@gmail.com>
2024-03-08 15:48:49 -0500

    Generating doc for Angular app

--- accordion-angular/src/app/video-player/common/utils/video-utils.ts
+++ accordion-angular/src/app/video-player/common/utils/video-utils.ts
@@ -1,5 +1,28 @@
 import { VIDEO_ASPECT_RATIO, MAX_VIDEO_WIDTH } from '../constants';
 
+/**
+ * Returns video container dimensions from inputs and
+ * maximum possible dimensions (browser limits)
+ *
+ * @param {number} inpWidth The specified width
+ * @param {number} inpHeight The specified height
+ * @param {number} maxWidth Maximum possible width
+ * @param {number} maxHeight Maximum possible height
+ * @returns {object} Contains width, height and top margin
+ *
+ * @example
+ * To adjust to specific width less than maximum width
+ * getVideoPlayerStyle(300, 0, 1000, 1000)
+ *
+ * @example
+ * To adjust to specific height less than maximum height
+ * getVideoPlayerStyle(0, 300, 1000, 1000)
+ *
+ * @example
+ * To adjust to maximum width or height
+ * getVideoPlayerStyle(0, 0, 1000, 1000)
+ *
+ */
 export function getVideoPlayerStyle(
   inpWidth: number,
   inpHeight: number,
