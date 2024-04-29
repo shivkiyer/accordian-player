3110ad202e9f19e17dbf06196f888b1f470f9ab8
Shivkumar Iyer <pythonpowerelectronics@gmail.com>
2024-04-29 16:58:51 -0400

    Volume control on video

--- src/video-player/components/video-player/video/video.jsx
+++ src/video-player/components/video-player/video/video.jsx
@@ -1,7 +1,11 @@
 import { useRef, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 
-import { selectVideoUrl, selectIsPlaying } from '../../../app/videoReducer';
+import {
+  selectVideoUrl,
+  selectIsPlaying,
+  selectVolume,
+} from '../../../app/videoReducer';
 import styles from './video.module.scss';
 
 /**
@@ -14,6 +18,7 @@ export default function Video() {
   const videoRef = useRef();
   const videoUrl = useSelector(selectVideoUrl);
   const isPlaying = useSelector(selectIsPlaying);
+  const volumeLevel = useSelector(selectVolume);
 
   /**
    * Handling pause/play from user control action
@@ -25,6 +30,13 @@ export default function Video() {
       videoRef.current.pause();
     }
   }, [isPlaying]);
+
+  /**
+   * Changing volume of video from user control action
+   */
+  useEffect(() => {
+    videoRef.current.volume = volumeLevel;
+  }, [volumeLevel]);
 
   return (
     <video
