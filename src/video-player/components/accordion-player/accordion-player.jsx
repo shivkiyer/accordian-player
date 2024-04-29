807b8180dbd854c5f62e70a240aac7f263f8af6c
Shivkumar Iyer <pythonpowerelectronics@gmail.com>
2024-04-29 10:17:40 -0400

    Moving params to root AccordionPlayer component

--- src/video-player/components/accordion-player/accordion-player.jsx
+++ src/video-player/components/accordion-player/accordion-player.jsx
@@ -5,13 +5,33 @@ import videoStore from './../../app/store';
 
 /**
  * Root Accordion Player component
- * 
+ *
+ * @param {number} width Width of the player in px (optional)
+ * @param {number} height Height of the player in px (optional)
+ * @param {string} url URL of video or config file (optional)
+ *
  * @returns {ReactNode} The Accordion Video Player
+ *
+ * @example
+ * <AccordionPlayer />
+ *
+ * @example
+ * <AccordionPlayer width='480' />
+ *
+ * @example
+ * <AccordionPlayer height='520' />
+ *
+ * @example
+ * <AccordionPlayer url='http://someurl.com' />
+ *
+ * @example
+ * <AccordionPlayer width='630' url='http://someurl.com' />
+ *
  */
-export default function AccordionPlayer() {
+export default function AccordionPlayer({ width, height, url }) {
   return (
     <Provider store={videoStore}>
-      <VideoPlayer />
+      <VideoPlayer width={width} height={height} url={url} />
     </Provider>
   );
 }
