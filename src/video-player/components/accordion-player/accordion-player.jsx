e07f0d83b37c2076c322bd87b8508e447c90d99e
Shivkumar Iyer <pythonpowerelectronics@gmail.com>
2024-05-02 11:39:43 -0400

    Moving root stylesheet in the app assets folder

--- src/video-player/components/accordion-player/accordion-player.jsx
+++ src/video-player/components/accordion-player/accordion-player.jsx
@@ -2,6 +2,7 @@ import { Provider } from 'react-redux';
 
 import VideoPlayer from '../video-player/video-player';
 import videoStore from './../../app/store';
+import './../../assets/styles/index.css';
 
 /**
  * Root Accordion Player component
