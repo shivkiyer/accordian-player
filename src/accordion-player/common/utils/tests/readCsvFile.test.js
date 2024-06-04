8843e6e67fe6f3e29a5fc6ca3a406b008d6f2e2c
Shivkumar Iyer <pythonpowerelectronics@gmail.com>
2024-06-04 18:51:58 -0400

    Tests for readCsv method

--- src/accordion-player/common/utils/tests/readCsvFile.test.js
+++ src/accordion-player/common/utils/tests/readCsvFile.test.js
@@ -1,6 +1,132 @@
+import testConfigData from './../../test-utils/getConfigData';
+
+/**
+ * Test for readCsv method
+ */
 describe('readCsv', () => {
-  // check if urls are playable, throw error otherwise
-  // check if number of frames is integer, throw error otherwise
-  // check if time instants are ok.
-  // check if background color is ok.
+  it('should return a data object with configuration properties', async () => {
+    const mockCheckVideoPlayable = (url) =>
+      Promise.resolve({ data: url, errMsg: null });
+    jest.mock('./../checkVideoPlayable', () => {
+      return mockCheckVideoPlayable;
+    });
+    const readCsv = require('./../readCsvFile').default;
+
+    const data = await readCsv(testConfigData());
+    expect(data).toHaveProperty('introVideo');
+    expect(data).toHaveProperty('framesPerSecond');
+    expect(data).toHaveProperty('backgroundColor');
+    expect(data).toHaveProperty('videoOptions');
+    expect(data).toHaveProperty('endscreenInfo');
+    expect(data).toHaveProperty('videoSequence');
+  });
+
+  it('should throw an error if videos cannot be played', async () => {
+    const mockCheckVideoPlayable = (url) => {
+      const result = new Promise((resolve, reject) => {
+        reject({ data: null, errMsg: 'Video cannot be played' });
+      });
+      return result;
+    };
+
+    jest.mock('./../checkVideoPlayable', () => {
+      return mockCheckVideoPlayable;
+    });
+
+    const readCsv = require('./../readCsvFile').default;
+
+    try {
+      const data = await readCsv(testConfigData());
+    } catch (e) {
+      expect(e).toHaveProperty('errMsg');
+      expect(e['errMsg']).toBe('Introduction video cannot be played.');
+    }
+  });
+
+  it('should throw and error if framesPerSecond is not an integer', async () => {
+    const mockCheckVideoPlayable = (url) =>
+      Promise.resolve({ data: url, errMsg: null });
+    jest.mock('./../checkVideoPlayable', () => {
+      return mockCheckVideoPlayable;
+    });
+    const readCsv = require('./../readCsvFile').default;
+
+    try {
+      const data = await readCsv(testConfigData({ framesPerSecond: 'a' }));
+    } catch (e) {
+      expect(e).toHaveProperty('errMsg');
+      expect(e['errMsg']).toBe('Frames per second must be integer');
+    }
+
+    const data = await readCsv(testConfigData({ framesPerSecond: 35.55 }));
+    expect(data).toHaveProperty('framesPerSecond');
+    expect(data['framesPerSecond']).toBe(35);
+  });
+
+  it('should throw an error if time instants are incorrect', async () => {
+    const mockCheckVideoPlayable = (url) =>
+      Promise.resolve({ data: url, errMsg: null });
+    jest.mock('./../checkVideoPlayable', () => {
+      return mockCheckVideoPlayable;
+    });
+    const readCsv = require('./../readCsvFile').default;
+
+    try {
+      const data = await readCsv(
+        testConfigData({ selectionStartInteraction: '00:05:05' })
+      );
+    } catch (e) {
+      expect(e).toHaveProperty('errMsg');
+      expect(e['errMsg']).toBe(
+        'START_INTERACTIVE_TIMESTAMP of selection video incorrect. Invalid time format - use the HH:MM:SS:FF (Hour:Minute:Second:Frame) format.'
+      );
+    }
+
+    try {
+      const data = await readCsv(
+        testConfigData({ selectionLoopTime: '0:x0:05:05' })
+      );
+    } catch (e) {
+      expect(e).toHaveProperty('errMsg');
+      expect(e['errMsg']).toBe(
+        'START_LOOP_TIMESTAMP of selection video incorrect. Invalid time format - parts of time instant HH:MM:SS:FF have to be integers.'
+      );
+    }
+
+    try {
+      const data = await readCsv(
+        testConfigData({ endingJumpTime: '0:00:05:35' })
+      );
+    } catch (e) {
+      expect(e).toHaveProperty('errMsg');
+      expect(e['errMsg']).toBe(
+        'JUMPTO_LOOP_TIMESTAMP of ending video incorrect. Invalid time format - frame in HH:MM:SS:FF must be less than 30.'
+      );
+    }
+  });
+
+  it('should throw and error if background color is invalid', async () => {
+    const mockCheckVideoPlayable = (url) =>
+      Promise.resolve({ data: url, errMsg: null });
+    jest.mock('./../checkVideoPlayable', () => {
+      return mockCheckVideoPlayable;
+    });
+    const readCsv = require('./../readCsvFile').default;
+
+    try {
+      const data = await readCsv(
+        testConfigData({ backgroundColor: 'fffffff' })
+      );
+    } catch (e) {
+      expect(e).toHaveProperty('errMsg');
+      expect(e['errMsg']).toBe('Background color is invalid');
+    }
+
+    try {
+      const data = await readCsv(testConfigData({ backgroundColor: 'ffffg' }));
+    } catch (e) {
+      expect(e).toHaveProperty('errMsg');
+      expect(e['errMsg']).toBe('Background color is invalid');
+    }
+  });
 });
